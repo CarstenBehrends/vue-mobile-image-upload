585195dddb67b9c0f85807c44791cb8ea29898b7
CarstenBehrends <c.behrends@flow4.com>
2019-04-24 14:18:12 +0200

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <image-input>
+      <div slot="activator">UPLOAD</div>
+    </image-input>
+  </div>
+</template>
+
+
+
+<script>
+import ImageInput from "../components/ImageInput.vue";
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  components: { ImageInput }
+};
+</script>
